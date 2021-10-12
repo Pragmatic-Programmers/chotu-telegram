e921baef2884dca85e7a128195f26934dbd11b46
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-10-12 11:55:34 +0530

    Bot launcher

---
+++ bot.py
@@ -0,0 +1,12 @@
+import logging
+import pyrogram
+
+
+# Setting up logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.__all__)
+
+

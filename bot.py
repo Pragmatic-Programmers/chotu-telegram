950613dd3fd734d70c318f0695525f45e3c9e63f
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-10-12 13:04:21 +0530

    Add start command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import pyrogram
+from config import Config
 
 
 # Setting up logging
@@ -7,6 +8,14 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.__all__)
+logging.getLogger("pyrogram").setLevel(logging.INFO)
 
+chotu = pyrogram.Client(
+    "chotu",
+    bot_token=Config.TOKEN,
+    api_id=Config.ID,
+    api_hash=Config.HASH,
+    plugins=dict(root="cogs")
+)
 
+chotu.run()

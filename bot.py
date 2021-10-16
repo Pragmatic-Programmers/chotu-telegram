466fef47de5a1224e945c4d22c5ebdc81b2395d3
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-10-16 15:22:56 +0530

    Modified logging Level

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 chotu = pyrogram.Client(
     "chotu",
